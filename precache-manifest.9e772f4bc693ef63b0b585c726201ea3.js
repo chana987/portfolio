747b79c1bf253d2293e6fae96abca231848aa24a
chana987 <chana987@gmail.com>
2020-02-11 17:17:32 +0200

    Updates

---
+++ precache-manifest.9e772f4bc693ef63b0b585c726201ea3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2423f133c4ed4147c49a84f6cd8629f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0f22f72362521f27dae9",
+    "url": "/portfolio/static/css/main.d4d980fd.chunk.css"
+  },
+  {
+    "revision": "87d19d117702bc03144d",
+    "url": "/portfolio/static/js/2.0c426b5f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.0c426b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f22f72362521f27dae9",
+    "url": "/portfolio/static/js/main.1671fc58.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "98a436904bad999ed65dc46cce7f677d",
+    "url": "/portfolio/static/media/carmel.98a43690.jpg"
+  },
+  {
+    "revision": "f3e0b8f6fcc0efd7f529286cfa932edc",
+    "url": "/portfolio/static/media/desert.f3e0b8f6.jpg"
+  },
+  {
+    "revision": "b8a0feaae3f864a26b18d540d885db3a",
+    "url": "/portfolio/static/media/profile.b8a0feaa.jpeg"
+  }
+]);
