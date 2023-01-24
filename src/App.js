132de486e44f8204dc38a0d47c399eda6179b37c
chana <chana987@gmail.com>
2023-01-24 21:39:58 +0200

    fixed style

--- src/App.js
+++ src/App.js
@@ -15,27 +15,27 @@ library.add(fab, faEnvelope, faPhone, faGlobe);
 const App = () => {
   return (
     <div id="app">
-      <div id="nav">
-        <div>
-          <img id="profile" src={ profile } alt="" />
-          <h3>Chana Meyer</h3>
-          <h4>Full-Stack Web Developer</h4>
-        </div>
-        <div id="links">
-          <NavLink className="nav-link" activeclassname="selected" to="/">Home</NavLink>
-          <NavLink className="nav-link" activeclassname="selected" to="/about">About Me</NavLink>
-          <NavLink className="nav-link" activeclassname="selected" to="/portfolio">Portfolio</NavLink>
-          <NavLink className="nav-link" activeclassname="selected" to="/gallery">Gallery</NavLink>
-          <NavLink className="nav-link" activeclassname="selected" to="/cv">CV</NavLink>
-        </div>
-      </div>
       <HashRouter>
+        <div id="nav">
+          <div>
+            <img id="profile" src={ profile } alt="" />
+            <h3>Chana Meyer</h3>
+            <h4>Full-Stack Web Developer</h4>
+          </div>
+          <div id="links">
+            <NavLink className="nav-link" activeclassname="selected" to="/">Home</NavLink>
+            <NavLink className="nav-link" activeclassname="selected" to="/about">About Me</NavLink>
+            <NavLink className="nav-link" activeclassname="selected" to="/portfolio">Portfolio</NavLink>
+            <NavLink className="nav-link" activeclassname="selected" to="/gallery">Gallery</NavLink>
+            <NavLink className="nav-link" activeclassname="selected" to="/cv">CV</NavLink>
+          </div>
+        </div>
         <Routes>
-          <Route exact path='/'><Home /></Route>
-          <Route exact path='/about'><About /></Route>
-          <Route exact path='/portfolio'><Portfolio /></Route>
-          <Route exact path='/gallery'><Gallery /></Route>
-          <Route exact path='/cv'><CV /></Route>
+          <Route exact path='/' element={ <Home /> } />
+          <Route exact path='/about' element={ <About /> } />
+          <Route exact path='/portfolio' element={ <Portfolio /> } />
+          <Route exact path='/gallery' element={ <Gallery /> } />
+          <Route exact path='/cv' element={ <CV /> } />
         </Routes>
       </HashRouter>
     </div>
