de3af11e67dc3eadfe5133fe3767ca588ee9f015
chana <chana987@gmail.com>
2023-01-24 21:09:59 +0200

    moved hashrouter

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React from 'react'
-import { HashRouter as Router, Route, NavLink, Routes } from 'react-router-dom'
-import About from './components/About'
-import Home from './components/Home'
-import Portfolio from './components/Portfolio'
-import Gallery from './components/Gallery'
-import CV from './components/CV'
-import profile from './assets/profile.jpeg'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { HashRouter, Route, NavLink, Routes } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Gallery from './components/Gallery';
+import CV from './components/CV';
+import profile from './assets/profile.jpeg';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fab, faEnvelope, faPhone, faGlobe)
+library.add(fab, faEnvelope, faPhone, faGlobe);
 
 const App = () => {
   return (
-    <Router>
-      <div id="app">
-        <div id="nav">
-          <div>
-            <img id="profile" src={profile} alt="" />
-            <h3>Chana Meyer</h3>
-            <h4>Full-Stack Web Developer</h4>
-          </div>
-          <div id="links">
-            <NavLink className="nav-link" activeclassname="selected" to="/">Home</NavLink>
-            <NavLink className="nav-link" activeclassname="selected" to="/about">About Me</NavLink>
-            <NavLink className="nav-link" activeclassname="selected" to="/portfolio">Portfolio</NavLink>
-            <NavLink className="nav-link" activeclassname="selected" to="/gallery">Gallery</NavLink>
-            <NavLink className="nav-link" activeclassname="selected" to="/cv">CV</NavLink>
-          </div>
+    <div id="app">
+      <div id="nav">
+        <div>
+          <img id="profile" src={ profile } alt="" />
+          <h3>Chana Meyer</h3>
+          <h4>Full-Stack Web Developer</h4>
         </div>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
-            <Route exact path='/portfolio' element={<Portfolio />} />
-            <Route exact path='/gallery' element={<Gallery />} />
-            <Route exact path='/cv' element={<CV />} />
-          </Routes>
+        <div id="links">
+          <NavLink className="nav-link" activeclassname="selected" to="/">Home</NavLink>
+          <NavLink className="nav-link" activeclassname="selected" to="/about">About Me</NavLink>
+          <NavLink className="nav-link" activeclassname="selected" to="/portfolio">Portfolio</NavLink>
+          <NavLink className="nav-link" activeclassname="selected" to="/gallery">Gallery</NavLink>
+          <NavLink className="nav-link" activeclassname="selected" to="/cv">CV</NavLink>
+        </div>
       </div>
-    </Router>
-  )
-}
+      <HashRouter>
+        <Routes>
+          <Route exact path='/' element={ <Home /> } />
+          <Route exact path='/about' element={ <About /> } />
+          <Route exact path='/portfolio' element={ <Portfolio /> } />
+          <Route exact path='/gallery' element={ <Gallery /> } />
+          <Route exact path='/cv' element={ <CV /> } />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
